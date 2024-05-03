bd87140fcb0533e5d3fb5ab3dede13daea10994f
Rincon <diego.rincon@wsu.edu>
2024-05-03 12:30:59 -0700

    added new analyses

--- code.R
+++ code.R
@@ -5,6 +5,7 @@ Aphid_2020FCl2 <- read.csv("./Data/year2020Val.csv") # Validation
 
 
 require(MASS)
+require(bbmle)
 
 # Modeling only degree-days
 
@@ -14,12 +15,21 @@ points(Aphid_2019FCl$Aphids_DD, (Aphid_2019FCl$CumAphid), col  = "red")
 
 degd <- c(Aphid_2022FCl$AphidDDS, Aphid_2020FCl$Aphids_DD, Aphid_2019FCl$Aphids_DD) # vector with degree-days for all years
 PrAphid <- c((Aphid_2022FCl$PrAphid), (Aphid_2020FCl$PrAphid), (Aphid_2019FCl$PrAphid)) # vector with proportion of aphids (out of the total per site) per site per sampling time for all years
+PrCAphid <- c((Aphid_2022FCl$CumAphid), (Aphid_2020FCl$CumAphid), (Aphid_2019FCl$CumAphid)) # vector with cumulative proportion of aphids (out of the total per site) per site per sampling time for all years
+
 
 aphDDs <- rep(degd, round(PrAphid * 1000)) # variable repeating degree-days weighted by proportions of aphids. This is required for parameter estimation of a gamma pdf
 
 modDDs <- fitdistr(aphDDs, "gamma")
 
-lines(seq(200, 1400), pgamma(seq(200, 1400), shape = coef(modDDs)[1], rate = coef(modDDs)[2])) # adds the line with the gamma model
+MLL_fun <- function(sh, rt) {
+  -sum(dgamma(dt, shape = sh, rate = rt, log = TRUE))
+}
+
+modelDDs <- mle2(MLL_fun, start = list(sh = coef(modDDs)[1], rt = coef(modDDs)[2]), data = list(dt = aphDDs))
+summary(modelDDs)
+
+lines(seq(200, 1400), pgamma(seq(200, 1400), shape = coef(modelDDs)[1], rate = coef(modelDDs)[2])) # adds the line with the gamma model
 points(Aphid_2020FCl2$Aphids_DD, (Aphid_2020FCl2$CumAphid), col = "brown", lwd = 2) # adds the "validation" points (from pea crops, not included in parameter estimation)
 
 
@@ -36,6 +46,51 @@ aphlight <- rep(light, round(PrAphid * 1000)) # variable repeating daylengths we
 
 modlgt <- fitdistr(aphlight, "gamma")
 
-lines(seq(14.5, 17, 0.1), pgamma(seq(14.5, 17, 0.1), shape = coef(modlgt)[1], rate = coef(modlgt)[2])) # adds the line with the gamma model
+modelLGT <- mle2(MLL_fun, start = list(sh = coef(modlgt)[1], rt = coef(modlgt)[2]), data = list(dt = aphlight))
+summary(modelLGT)
+
+lines(seq(14.5, 17, 0.1), pgamma(seq(14.5, 17, 0.1), shape = coef(modelLGT)[1], rate = coef(modelLGT)[2])) # adds the line with the gamma model
 points(Aphid_2020FCl2$dlD, (Aphid_2020FCl2$CumAphid), col = "brown", lwd = 2) # adds the "validation" points (from pea crops, not included in parameter estimation)
 
+require(piecewiseSEM)
+
+data_complete <- data.frame(AphidC = PrAphid, cums = PrCAphid, DDs = degd, light  = light)
+
+sem1 <- psem(glm(AphidC ~ DDs + light, data = data_complete, family = "binomial"))
+summary(sem1)
+
+sem2 <- psem(gam(cums ~ DDs + light, data = data_complete, family = "binomial"))
+summary(sem2)
+
+
+require(randomForest)
+
+mod_RF <- randomForest(cums ~ DDs + light, data = data_complete, na.action = na.omit)
+mod_RF
+varImpPlot(mod_RF)
+
+mod_RF1 <- randomForest(cums ~ DDs, data = data_complete, na.action = na.omit)
+mod_RF1
+
+mod_RF2 <- randomForest(cums ~ light, data = data_complete, na.action = na.omit)
+mod_RF2
+
+
+PC_mod <- prcomp(as.matrix(data_complete[, -c(1, 2)]), scale. = TRUE, retx = TRUE)
+PC_mod
+fviz_eig(PC_mod)
+get_eigenvalue(PC_mod)
+
+
+plot(PC_mod$x[, 1]+3, PrCAphid)
+
+PC1_comb <- rep(PC_mod$x[, 1]+3, round(PrAphid * 1000))
+
+modPC1 <- fitdistr(PC1_comb, "gamma")
+
+modelComb <- mle2(MLL_fun, start = list(sh = coef(modPC1)[1], rt = coef(modPC1)[2]), data = list(dt = PC1_comb))
+summary(modelComb)
+lines(seq(0, 6, 0.1), pgamma(seq(0, 6, 0.1), shape = coef(modelComb)[1], rate = coef(modelComb)[2]))
+
+anova(modelDDs, modelLGT, modelComb)
+AICtab(modelDDs, modelLGT, modelComb)
